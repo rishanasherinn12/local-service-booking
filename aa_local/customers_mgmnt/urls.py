b42a99e423eee47d88163162ee4bad1d750871e8
rishana sherin <rishanasherinn12@gmail.com>
2026-02-12 23:58:22 +0530

    Listed Customers in Admin panel

--- aa_local/customers_mgmnt/urls.py
+++ aa_local/customers_mgmnt/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.admin_customers, name = 'admin_customers'),
+    path('block/<int:user_id>/', views.block_customer, name = 'block_customer'),
+    path('uunblock/<int:user_id>/', views.unblock_customer, name = 'unblock_customer'),
     
 ]
--- aa_local/customers_mgmnt/views.py
+++ aa_local/customers_mgmnt/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Count
 from django.contrib.auth.models import User
 from accounts.models import CustomerProfile
+from django.contrib import messages
 
 # Create your views here.
 def admin_customers(request):
@@ -11,3 +12,18 @@ def admin_customers(request):
         CustomerProfile.objects.get_or_create(user=user, defaults={"full_name":user.get_full_name() or user.username})
 
     return render(request,'admin/customers_mgnt/admin_customers.html',{'customers':customers})
+
+
+def block_customer(request,user_id):
+    user = get_object_or_404(User, id=user_id)
+    user.is_active = False
+    user.save()
+    messages.success(request,'Customer blocked successfully')
+    return redirect('block_customer')
+
+def unblock_customer(request,user_id):
+    user = get_object_or_404(User, id=user_id)
+    user.is_active = True
+    user.save()
+    messages.success(request, "Customer unblocked successfully")
+    return redirect('unblock_customer')
