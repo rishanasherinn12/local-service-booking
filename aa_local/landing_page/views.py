30eb8ec7256fcaa4d4b1a2c42847aa92cfd41450
rishana sherin <rishanasherinn12@gmail.com>
2026-03-11 23:45:02 +0530

    working on landing page

--- aa_local/landing_page/views.py
+++ aa_local/landing_page/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render
 from services.models import ServiceCategory, Service, Worker
 from bookings.models import Booking, Review
-from django.db.models import Avg
+from django.db.models import Count, Avg
 
 
 # Create your views here.
 def home(request):
     categories = ServiceCategory.objects.filter(is_active = True)[:4]
-    best_services = Service.objects.filter(is_active=True,is_best_seller=True)[:6]
+    best_services = Service.objects.filter(is_active=True).annotate(total_bookings=Count("bookings")).order_by("-total_bookings")[:3]
     top_workers = Worker.objects.filter(is_active=True).order_by("-rating")[:4]
     total_customers = Booking.objects.values("customer").distinct().count()
     total_bookings = Booking.objects.count()
-
-    best_services = Service.objects.filter(
-    is_active=True,
-    is_best_seller=True
-    ).select_related("category")[:3]
     
     context={
         "categories": categories,
@@ -23,6 +18,5 @@ def home(request):
         "top_workers": top_workers,
         "total_customers": total_customers,
         "total_bookings": total_bookings,
-        "best_services": best_services,
     }
     return render (request,'pages/index.html',context)
